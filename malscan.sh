88b0b8897ed18d5585d64e84b9cd2071ef88f0c6
commandrine <commandrine@gmail.com>
2026-09-09 13:47:29 +0800

    Revision
    Added command to list Maldet reports after scanning.

--- malscan.sh
+++ malscan.sh
@@ -1,23 +1,13 @@
+#!/usr/bin/env bash
 # Update ClamAV signatures
-
 sudo freshclam
 
- 
-
 # Update Maldet
-
 sudo maldet --update-ver
-
 sudo maldet --update-sigs
 
- 
-
 # Scan home directory
-
 sudo maldet --scan-all /home
 
- 
-
 # Review findings manually
-
 sudo maldet --report list
